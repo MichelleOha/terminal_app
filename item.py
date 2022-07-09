e90996684c3a28e0985fdfbfd79fc6dde4a96ea0
Michelle Ohannessian <michelleohannessian@Michelles-MBP.modem>
2022-07-09 12:11:37 +1000

    created class item

---
+++ item.py
@@ -0,0 +1,17 @@
+
+class Item:
+    # never call init, it's the method that sets the inital attrributes to the class
+    def __init__(self, description, style, size, category, price):
+        self.description = description  # name value of the attribute
+        self.style = style
+        self.size = size
+        self.category = category
+        self.price = price
+
+    def show_item(self):
+        print(
+            f"{self.description}: {self.style}: {self.size}: {self.category}: ${self.price}")
+
+
+jeans = Item("boyfriend", "jeans", 12, "bottoms", 99.99)
+jeans.show_item()
