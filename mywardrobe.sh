0ea2f046fef568cf3c9271d8c2f3ae846a20ec51
Michelle Ohannessian <michelleohannessian@Michelles-MBP.modem>
2022-07-15 15:43:19 +1000

    started writing to my README.md

--- mywardrobe.sh
+++ mywardrobe.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-if ! [[ -x "$(command -v python3)"]]
-then
-    echo 'Python not installed:
-      Please install Python3 by visiting this website https://installpython3.com which will then allow you to run the application.'
-    exit 1
+if ! [[ -x "$(command -v python)"]]: then
+   echo 'Python not installed: Please install Python3 by visiting this website https://installpython3.com which will then allow you to run the application.'
+   exit 1
 fi
 
-if ! [[ $# -eq 1 ]]: then
-    echo 'View the help manual'
-else
     python3 mywardrobe.py
-fi
+
