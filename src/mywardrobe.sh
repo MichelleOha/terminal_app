d300f66e62990ac09199fe9730f6f0ce35bceb48
Michelle Ohannessian <michelleohannessian@Michelles-MBP.modem>
2022-07-16 10:35:31 +1000

    Bash script fixed and README almost completed

---
+++ src/mywardrobe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! [[ -x "$(command -v python3)" ]] 
+then
+  echo 'Python not installed: Please install Python3 by visiting this website https://installpython3.com which will then allow you to run the application.'
+  exit 1
+fi
+
+    python3 main.py
+
---
+++ src/read_write.py
@@ -0,0 +1,44 @@
+import json
+
+# this class will read and write to a json file (store data)
+
+
+class ReadWrite:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def get_json_list(self):
+        try:
+            input_file = open(self.file_path)
+            json_list = json.load(input_file)
+            return json_list
+        except:
+            raise IOError(
+                "There was an issue reading your list.json file. Please make sure it exists and is in the correct format.")
+
+    def write_item_to_json(self, item):
+        try:
+            input_file = open(self.file_path)
+            json_list = json.load(input_file)
+            json_list.append(item)
+            with open(self.file_path, 'w') as my_file:
+                json.dump(json_list, my_file)
+        except:
+            raise IOError(
+                "There was an issue reading your list.json file. Please make sure it exists and is in the correct format.")
+
+    def write_list_to_json(self, list):
+        try:
+            with open(self.file_path, 'w') as my_file:
+                json.dump(list, my_file)
+        except:
+            raise IOError(
+                "There was an issue reading your list.json file. Please make sure it exists and is in the correct format.")
+
+    def reset_json(self):
+        try:
+            with open(self.file_path, 'w') as my_file:
+                json.dump([], my_file)
+        except:
+            raise IOError(
+                "There was an issue reading your list.json file. Please make sure it exists and is in the correct format.")
